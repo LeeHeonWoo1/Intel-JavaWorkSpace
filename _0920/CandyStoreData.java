965a86143ae105cb5fd36e072e80cfd2fee991c6
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-21 16:23:07 +0900

    기존 클래스 수정

--- _0920/CandyStoreData.java
+++ _0920/CandyStoreData.java
@@ -1,25 +1,57 @@
 package _0920;
 
 public class CandyStoreData {
-    private static int id = 0;
+    private static int cusCnt = 0;
     private int cusNum;
     private String name;
-    private String date;
+    private String joinDate;
+    private String telNum;
     private int age;
-    private String phoneNum;
-    private String account;
+    private String accNum;
 
-    public void setData(String name, String date, String phoneNum, int age, String account){
-        this.cusNum = ++id;
+    public void setData(String name, String date,
+                        String tel, int age, String acc) {
+        this.cusNum = ++cusCnt;
         this.name = name;
-        this.date = date;
+        this.joinDate = date;
+        this.telNum = tel;
         this.age = age;
-        this.phoneNum = phoneNum;
-        this.account = account;
+        this.accNum = acc;
     }
 
-    public String getData(){
-        String newVal = cusNum + "\t\t" + name + "\t" + date + "\t" + phoneNum + "\t" + age + "\t" + account;
-        return newVal;
+    public String getAllData() {
+        return this.cusNum + "\t" + this.name + "\t" + this.joinDate
+                + "\t" + this.telNum + "\t" + this.age + "\t" + this.accNum;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setJoinDate(String date) {
+        this.joinDate = date;
+    }
+    public String getJoinDate() {
+        return this.joinDate;
+    }
+    public void setTelNum(String tel) {
+        this.telNum = tel;
+    }
+    public String getTelNum() {
+        return this.telNum;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setAccNum(String accNum) {
+        this.accNum = accNum;
+    }
+    public String getAccNum() {
+        return this.accNum;
     }
 }
