f8d88b236204d1a12112d69b062b62bca666a208
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-21 15:44:42 +0900

    About getter setter

---
+++ _0920/GetterSetter.java
@@ -0,0 +1,22 @@
+package _0920;
+
+public class GetterSetter {
+    private int x;
+    private int y;
+
+
+    public void setX(int a){
+        this.x = a;
+    }
+
+    public void setY(int a){
+        this.y = a;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+}
