625d1da9db9e68fdc1c51db90fe0b8b2204bf9bc
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-21 15:45:18 +0900

    use final keyword with static

---
+++ _0920/UseFinalStatic.java
@@ -0,0 +1,10 @@
+package _0920;
+
+public class UseFinalStatic {
+    public static void main(String[] args) {
+        FinalStatic fs = new FinalStatic("이헌우");
+        fs.myName2 = "윈터";
+        System.out.println(fs.myName);
+        System.out.println(fs.myName2);
+    }
+}
