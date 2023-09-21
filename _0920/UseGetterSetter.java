c10e1df844361bf8da47d0dc4dbad7e5696d9c38
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-21 15:45:28 +0900

    use getter setter

---
+++ _0920/UseGetterSetter.java
@@ -0,0 +1,12 @@
+package _0920;
+
+public class UseGetterSetter {
+    public static void main(String[] args) {
+        GetterSetter getterSetterr = new GetterSetter();
+
+        getterSetterr.setX(2);
+        getterSetterr.setY(3);
+
+        System.out.println(getterSetterr.getX() + " " + getterSetterr.getY());
+    }
+}
