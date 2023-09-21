39ca3bdf5041efdc61640456eeacdde982ccaa90
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-21 16:23:24 +0900

    직원 부분 추가

--- _0920/UseCandy.java
+++ _0920/UseCandy.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class UseCandy {
     public static void main(String[] args) {
         CandyStoreCustomer[] cd = new CandyStoreCustomer[5];
+        CandyStoreEmployee[] ce = new CandyStoreEmployee[3];
         int inputCnt = 0;
 
-        for (int i = 0; i < cd.length; i++) {
+        for (int i = 0; i < cd.length; i++)
             cd[i] = new CandyStoreCustomer();
-        }
+        for (int i = 0; i < ce.length; i++)
+            ce[i] = new CandyStoreEmployee();
+
 
         cd[0].setData("이헌우", "2010/05/01", "010-1111-2222", 24, "121212-34343434");
         cd[1].setData("주재원", "2009/07/03", "010-1234-5678", 24, "765455-99887766");
         cd[2].setData("이유환", "2007/04/11", "010-3232-8787", 25, "635675-22554433");
         cd[3].setData("박현우", "2004/12/08", "010-7766-4422", 24, "385451-54638991");
         cd[4].setData("신성영", "2003/06/22", "010-7755-6543", 28, "325648-94684681");
+
+        ce[0].setInfo("박현우", "2020/06/12", "010-5555-6666", 24, "123123-11223344", "사장", "경영");
+        ce[1].setInfo("카리나", "2000/01/02", "010-5422-8764", 22, "653544-9876346", "부장", "카운터");
+        ce[2].setInfo("김민정", "2000/02/04", "010-3426-8637", 22, "876546-2332329", "직원", "매장관리");
         Scanner intScanner = new Scanner(System.in);
         Scanner strScanner = new Scanner(System.in);
 
@@ -31,7 +38,7 @@ public class UseCandy {
                 case 1:
                     System.out.println("고객번호\t고객이름\t가입날짜\t\t전화번호\t\t\t나이\t계좌번호");
                     for(int i = 0;i < cd.length;i++)
-                        System.out.println(cd[i].getData());
+                        System.out.println(cd[i].getAllData());
 
                     break;
                 case 2:
@@ -39,8 +46,8 @@ public class UseCandy {
                     String key = strScanner.nextLine();
                     System.out.println("=================================================================");
                     for(int i = 0;i < cd.length;i++) {
-                        if (cd[i].getData().contains(key)) {
-                            System.out.println(cd[i].getData());
+                        if (cd[i].getAllData().contains(key)) {
+                            System.out.println(cd[i].getAllData());
                         }
                     }
                     break;
@@ -90,7 +97,7 @@ public class UseCandy {
                     int price = intScanner.nextInt();
 
                     for (int i = 0; i < cd.length; i++) {
-                        if (cd[i].getData().contains(cusName)){
+                        if (cd[i].getAllData().contains(cusName)){
                             cd[i].setPoint(price);
                         }
                     }
@@ -100,7 +107,7 @@ public class UseCandy {
                     System.out.print("조회하려는 고객의 이름을 입력하세요 : ");
                     String cusName2 = strScanner.nextLine();
                     for (int i = 0; i < cd.length; i++) {
-                        if (cd[i].getData().contains(cusName2)){
+                        if (cd[i].getAllData().contains(cusName2)){
                             cd[i].getPoint();
                         }
                     }
