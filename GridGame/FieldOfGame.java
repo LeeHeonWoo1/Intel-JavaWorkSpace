1c96a8bc80be16f616c8d00e474070cc7bc57c14
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-26 15:40:44 +0900

    맵을 가리고, 현재 위치에서 1칸 내에 어떤 요인이 있는지 조건 추가

--- GridGame/FieldOfGame.java
+++ GridGame/FieldOfGame.java
@@ -1,11 +1,12 @@
 package GridGame;
 
 public class FieldOfGame {
-    String[][] feild = new String[30][40];
+    static String[][] feild = new String[30][40];
     static int locationX = 15;
     static int locationY = 0;
     static int userHp = 100;
-    String swap;
+    static int exp = 0;
+    static int point = 0;
 
     public void setFeild(){
         for (int i = 0; i < feild.length; i++) {
@@ -39,7 +40,7 @@ public class FieldOfGame {
 
     public void moveY(int y, int sight){
         if (sight == 3){
-            if (locationX >= feild.length){
+            if (locationX > feild.length){
                 System.out.println("벽에 가로막혀 지나갈 수 없습니다.");
             }else{
                 feild[locationX][locationY] = "🟦";
@@ -58,7 +59,7 @@ public class FieldOfGame {
     }
     public void moveX(int x, int sight){
         if (sight == 2){
-            if (locationY >= feild[0].length){
+            if (locationY > feild[0].length){
                 System.out.print("벽에 가로막혀 지나갈 수 없습니다.");
             }else{
                 feild[locationX][locationY] = "🟦";
@@ -74,5 +75,33 @@ public class FieldOfGame {
                 locationY -= x;
             }
         }
+    }
+
+    public void getStation(){
+        System.out.println("-------------상황발생-------------");
+        if (locationY == feild[0].length){
+            System.out.println("전방은 막혀있는 상태입니다.");
+        }else if (!(feild[locationX][locationY+1].equals("🟦"))){
+            System.out.println("전방에 특별 요인이 존재합니다.");
+        }
+
+        if (locationY == 0){
+            System.out.println("후방은 막혀있는 상태입니다.");
+        }else if (!(feild[locationX][locationY-1].equals("🟦"))){
+            System.out.println("후방에 특별 요인이 존재합니다.");
+        }
+
+        if (locationX == feild.length){
+            System.out.println("우측은 막혀있는 상태입니다.");
+        }else if (!(feild[locationX+1][locationY].equals("🟦"))){
+            System.out.println("우측에 특별 요인이 존재합니다.");
+        }
+
+        if (locationX == 0){
+            System.out.println("좌측은 막혀있는 상태입니다.");
+        }else if (!(feild[locationX-1][locationY].equals("🟦"))){
+            System.out.println("좌측에 특별 요인이 존재합니다.");
+        }
+        System.out.println("---------------------------------");
     }
 }
