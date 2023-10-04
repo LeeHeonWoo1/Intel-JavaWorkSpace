1255bcd759b5f070c4d9100668a4b2a44f4bd4d3
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 13:47:12 +0900

    default method추가, static method 추가

--- _1004/MakeInterface.java
+++ _1004/MakeInterface.java
@@ -9,9 +9,29 @@ public interface MakeInterface {
     int MIN_POWER_LEVEL = 0;
 
     // 중괄호를 열어 내부에 작동 코드를 넣었던 방식과는 다르게 인터페이스에서 메소드를 정의하는 경우 양식만 지정하면 된다.
+    // 즉, 인터페이스는 구현 클래스가 재정의해야하는 public 추상 메소드를 가진다.
     public void turnOn();
     public void turnOff();
     public void powerUp();
     public void powerDown();
     public void powerSet(int level);
+    public boolean getStation();
+    public void setStation(boolean f);
+
+    // 위 메소드들은 실행부가 없는 추상 메소드이지만, 실행부를 가지는 default 메소드도 가질 수 있다.
+    default boolean setAirDirection(boolean station){
+        if (station){
+            System.out.println("회전을 멈춥니다.");
+            station = false;
+        }else{
+            System.out.println("회전을 시작합니다.");
+            station = true;
+        }
+
+        return station;
+    }
+
+    static void speakBook(){
+        System.out.println("저는 예약 기능이 없습니다.");
+    }
 }
