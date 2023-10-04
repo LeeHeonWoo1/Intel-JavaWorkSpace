0a7a579ba6038e6037c17615d999dfe1b1a2aa39
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 11:10:15 +0900

    클래스 수정

--- _1004/AirConditioner.java
+++ _1004/AirConditioner.java
@@ -1,6 +1,10 @@
 package _1004;
 
+import java.util.Scanner;
+
 public class AirConditioner implements MakeInterface{
+    int temp = 24;
+    Scanner intScanner = new Scanner(System.in);
 
     public void turnOn(){
         System.out.println("에어컨을 작동합니다.");
@@ -8,5 +12,21 @@ public class AirConditioner implements MakeInterface{
 
     public void turnOff(){
         System.out.println("에어컨을 끕니다.");
+    }
+
+    public void powerSet(int newTemp){
+        System.out.printf("에어컨의 온도를 %d도로 설정합니다.\n", newTemp);
+    }
+
+    public void powerUp(){
+        System.out.print("낮출 온도를 작성하세요 : ");
+        temp = intScanner.nextInt();
+        System.out.printf("에어컨의 온도를 %d도로 낮춥니다.\n", temp);
+    }
+
+    public void powerDown(){
+        System.out.print("높일 온도를 작성하세요 : ");
+        temp = intScanner.nextInt();
+        System.out.printf("에어컨의 온도를 %d도로 높입니다.\n", temp);
     }
 }
