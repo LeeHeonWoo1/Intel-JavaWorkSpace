0d57f3c89e434008d282f7218433f2af8f462729
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 10:20:52 +0900

    인터페이스 사용하기

---
+++ _1004/AirConditioner.java
@@ -0,0 +1,12 @@
+package _1004;
+
+public class AirConditioner implements MakeInterface{
+
+    public void turnOn(){
+        System.out.println("에어컨을 작동합니다.");
+    }
+
+    public void turnOff(){
+        System.out.println("에어컨을 끕니다.");
+    }
+}
