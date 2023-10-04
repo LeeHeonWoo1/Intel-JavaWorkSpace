d05d56e951692ac9f8796e553427a028b80d8824
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 13:47:40 +0900

    상태 getter setter 추가

--- _1004/AirConditioner.java
+++ _1004/AirConditioner.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AirConditioner implements MakeInterface{
     int temp = 24;
     Scanner intScanner = new Scanner(System.in);
+    boolean isDirect = false;
 
     public void turnOn(){
         System.out.println("에어컨을 작동합니다.");
@@ -28,5 +29,13 @@ public class AirConditioner implements MakeInterface{
         System.out.print("높일 온도를 작성하세요 : ");
         temp = intScanner.nextInt();
         System.out.printf("에어컨의 온도를 %d도로 높입니다.\n", temp);
+    }
+
+    public boolean getStation(){
+        return isDirect;
+    }
+
+    public void setStation(boolean f){
+        isDirect = f;
     }
 }
