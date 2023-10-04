61f7e14f975755f2af37f65df7dc4d566f12a804
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 10:21:07 +0900

    인터페이스를 사용해 만든 클래스 객체 정의

---
+++ _1004/UseFan.java
@@ -0,0 +1,18 @@
+package _1004;
+
+public class UseFan {
+    public static void main(String[] args) {
+        // 인터페이스에서 정의한 형상에 맞게 구현한 클래스를 사용하려면 클래스로 객체를 생성하는게 아니라
+        // 인터페이스 이름으로 객체를 생성해야 한다.
+        MakeInterface cooler;
+
+        cooler = new Fan();
+        cooler.turnOn();
+        cooler.turnOff();
+
+        cooler = new AirConditioner();
+        cooler.turnOn();
+        cooler.turnOff();
+
+    }
+}
