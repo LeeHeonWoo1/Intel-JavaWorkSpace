33acfbd282956ffe6ee05cdef06f26a6f535d2f4
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-04 13:56:49 +0900

    main method of multi implement

---
+++ _1004/UseMultiImplement.java
@@ -0,0 +1,12 @@
+package _1004;
+
+public class UseMultiImplement {
+    public static void main(String[] args) {
+        MultiImplements newInterFace = new MultiImplements();
+
+        newInterFace.add(5, 8);
+        newInterFace.sub(5, 8);
+        newInterFace.mul(5, 8);
+        newInterFace.div(5, 8);
+    }
+}
