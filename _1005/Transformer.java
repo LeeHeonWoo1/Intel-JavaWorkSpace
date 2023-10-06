75052ddd60c270b816af8cb9fb90f0c330075820
LeeHeonWoo1 <gjsdn245@naver.com>
2023-10-06 10:08:13 +0900

    16진수 변환 메소드 추가, 역변환 추가

--- _1005/Transformer.java
+++ _1005/Transformer.java
@@ -1,16 +1,71 @@
 package _1005;
 
 public class Transformer {
-    public void transform(int x, int userInput){
+    public char[] transform(int x, int userInput) {
         String result = "";
 
-        while(userInput >= 1){
-            result += userInput % x;
-            userInput /= x;
+        if (x == 2 || x == 8) {
+            while (userInput >= 1) {
+                result += userInput % x;
+                userInput /= x;
+            }
+        } else {
+            while (userInput >= 1) {
+                int remain = userInput % 16;
+
+                if (remain >= 10)
+                    result += (char) (remain + 55);
+                else
+                    result += remain;
+
+                userInput /= 16;
+            }
         }
 
         char[] arr = result.toCharArray();
-        for (int i = arr.length - 1; i >=0  ; i--)
+        for (int i = arr.length - 1; i >= 0; i--)
             System.out.print(arr[i]);
+
+        return arr;
+    }
+
+    public int power(int x, int y) {
+        int result = 1;
+
+        if (y == 0)
+            return 1;
+
+        for (int i = 0; i < y; i++)
+            result *= x;
+
+        return result;
+    }
+
+    public void inverseTransform(int x, String userInput) {
+        int result = 0;
+        char[] arr = userInput.toCharArray();
+
+        int idx = arr.length - 1;
+
+        if (x == 2 || x == 8) {
+            for (int i = 0; i < arr.length; i++) { // 배열의 길이만큼 순회하면서
+                if ((int)(arr[idx]) - 48 != 0)
+                    result += power(x, i) * ((int)(arr[idx]) - 48);
+
+                idx--;
+            }
+        }else{
+            for (int i = 0; i < arr.length; i++) { // 배열의 길이만큼 순회하면서
+                if ((int) (arr[idx]) >= 65) {
+                    result += power(x, i) * ((int) (arr[idx]) - 55);
+                    idx--;
+                }else{
+                    result += power(x, i) * ((int) (arr[idx]) - 48);
+                    idx--;
+                }
+            }
+        }
+
+        System.out.println(result);
     }
 }
